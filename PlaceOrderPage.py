f44e1ed9438608c6945375d1748a8315861fb7ca
Zoupi1 <47620170+Zoupi1@users.noreply.github.com>
2020-12-16 22:58:57 +0100

    nextID fixed

--- PlaceOrderPage.py
+++ PlaceOrderPage.py
@@ -81,29 +81,34 @@ def checkOut():
                 outOfStock = []
                 try:
                     # Hitta nästa auto_increment på OrderID
-                    cursor.execute(
-                        'SELECT AUTO_INCREMENT FROM information_schema.TABLES WHERE (TABLE_NAME = "Orders");')
-                    nextID = cursor.fetchone()
+                    #cursor.execute(
+                    #    'SELECT AUTO_INCREMENT FROM information_schema.TABLES WHERE (TABLE_NAME = "Orders");')
+                    #nextID = cursor.fetchone()
 
                     # Skapa en ny Order
                     cursor.execute(
                         'INSERT INTO `Orders`(`UserID`, `Amount`, `OrderStatus`,`FirstName`, `LastName`,`ShippingAdress`, `OrderPhoneNumber`, `OrderEmail`) VALUES (%s,%s,%s,%s,%s,%s,%s,%s)',
                         (userid, price, "Processing Order", lista[0], lista[1], lista[4], lista[3], lista[2],))
-
+                    
+                    
+                    # Hitta nästa auto_increment på OrderID
+                    nextID = cursor.lastrowid
+                    print(nextID)
+                    print("################################################")
                     for x in prods:  # kopiera över allt från cart till orderdetails
                         cursor.execute('SELECT * FROM Products WHERE ProductID= %s ', (x['ProductsID'],))
                         a = cursor.fetchone()
                         if int(a['NumberInStock']) >= x['Amount']:
                             cursor.execute('UPDATE Products SET NumberInStock=%s WHERE ProductID= %s ',
-                                           (int(a['NumberInStock']) - (int(x['Amount'])), x['ProductsID'],))
+                                            (int(a['NumberInStock']) - (int(x['Amount'])), x['ProductsID'],))
                         else:
                             if a not in outOfStock:
                                 outOfStock.append(a)
                         cursor.execute(
                             'INSERT INTO `OrderDetails`( `OrderID`, `ProductID`,`BuyingPrice` ,`Amount`) VALUES (%s,%s,%s,%s)',
-                            (nextID['AUTO_INCREMENT'], x['ProductsID'], a['ProductPrice'], 1,))
+                            (nextID, x['ProductsID'], a['ProductPrice'], 1,))
                         cursor.execute('DELETE FROM Cart WHERE UserID =%s and ProductsID = %s',
-                                       (userid, x['ProductsID'],))
+                                        (userid, x['ProductsID'],))
                     if len(outOfStock) == 0:
                         mysql.connection.commit()
                     else:
@@ -116,13 +121,12 @@ def checkOut():
                                 message += x['ProductName']
                         session['orderMsg'] = message
 
-                    url = "/place-order." + str(nextID['AUTO_INCREMENT'])
+                    url = "/place-order." + str(nextID)
                     return redirect(url, code=302)
                 except:
                     flash("Något fel hände :( Testa gärna igen!")
                     mysql.connection.rollback()
                 finally:
-                    print("FINALLY")
                     cursor.close()
             else:
                 return "<h1>Din cart är tom</h1>"
